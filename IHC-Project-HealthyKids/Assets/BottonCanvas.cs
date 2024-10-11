e4252580a9b13cc1e58107b8cde1377ae606ba23
Alexis Raza <gilver.raza@utec.edu.pe>
2024-10-11 16:35:23 -0500

    update interfas / .gitignore
    interfaz menú

---
+++ IHC-Project-HealthyKids/Assets/BottonCanvas.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottonCanvas : MonoBehaviour
+{
+    public void Menu()
+    {
+        Debug.Log("Menu");
+    }
+}
